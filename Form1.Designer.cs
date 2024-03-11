ca1359ca32248900fbdfbe33c25d7c0c62034232
Douglas Morales <DouGMoralesVelasquez@hotmail.com>
2024-03-11 08:48:38 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace LabRepaso1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewAsistencia = new System.Windows.Forms.DataGridView();
+            this.dataGridViewEmpleados = new System.Windows.Forms.DataGridView();
+            this.buttonLeerDatos = new System.Windows.Forms.Button();
+            this.dataGridViewResultado = new System.Windows.Forms.DataGridView();
+            this.buttonCalcular = new System.Windows.Forms.Button();
+            this.comboBoxEmpleados = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAsistencia)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmpleados)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResultado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewAsistencia
+            // 
+            this.dataGridViewAsistencia.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewAsistencia.Location = new System.Drawing.Point(508, 79);
+            this.dataGridViewAsistencia.Name = "dataGridViewAsistencia";
+            this.dataGridViewAsistencia.RowHeadersWidth = 51;
+            this.dataGridViewAsistencia.RowTemplate.Height = 24;
+            this.dataGridViewAsistencia.Size = new System.Drawing.Size(402, 281);
+            this.dataGridViewAsistencia.TabIndex = 0;
+            // 
+            // dataGridViewEmpleados
+            // 
+            this.dataGridViewEmpleados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEmpleados.Location = new System.Drawing.Point(43, 79);
+            this.dataGridViewEmpleados.Name = "dataGridViewEmpleados";
+            this.dataGridViewEmpleados.RowHeadersWidth = 51;
+            this.dataGridViewEmpleados.RowTemplate.Height = 24;
+            this.dataGridViewEmpleados.Size = new System.Drawing.Size(409, 281);
+            this.dataGridViewEmpleados.TabIndex = 1;
+            // 
+            // buttonLeerDatos
+            // 
+            this.buttonLeerDatos.Location = new System.Drawing.Point(539, 394);
+            this.buttonLeerDatos.Name = "buttonLeerDatos";
+            this.buttonLeerDatos.Size = new System.Drawing.Size(89, 44);
+            this.buttonLeerDatos.TabIndex = 2;
+            this.buttonLeerDatos.Text = "Leer Datos";
+            this.buttonLeerDatos.UseVisualStyleBackColor = true;
+            this.buttonLeerDatos.Click += new System.EventHandler(this.buttonLeerDatos_Click);
+            // 
+            // dataGridViewResultado
+            // 
+            this.dataGridViewResultado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewResultado.Location = new System.Drawing.Point(969, 79);
+            this.dataGridViewResultado.Name = "dataGridViewResultado";
+            this.dataGridViewResultado.RowHeadersWidth = 51;
+            this.dataGridViewResultado.RowTemplate.Height = 24;
+            this.dataGridViewResultado.Size = new System.Drawing.Size(369, 281);
+            this.dataGridViewResultado.TabIndex = 3;
+            // 
+            // buttonCalcular
+            // 
+            this.buttonCalcular.Location = new System.Drawing.Point(771, 394);
+            this.buttonCalcular.Name = "buttonCalcular";
+            this.buttonCalcular.Size = new System.Drawing.Size(126, 42);
+            this.buttonCalcular.TabIndex = 4;
+            this.buttonCalcular.Text = "Calcular Sueldo";
+            this.buttonCalcular.UseVisualStyleBackColor = true;
+            this.buttonCalcular.Click += new System.EventHandler(this.buttonCalcular_Click);
+            // 
+            // comboBoxEmpleados
+            // 
+            this.comboBoxEmpleados.FormattingEnabled = true;
+            this.comboBoxEmpleados.Location = new System.Drawing.Point(43, 26);
+            this.comboBoxEmpleados.Name = "comboBoxEmpleados";
+            this.comboBoxEmpleados.Size = new System.Drawing.Size(409, 24);
+            this.comboBoxEmpleados.TabIndex = 5;
+            this.comboBoxEmpleados.SelectedIndexChanged += new System.EventHandler(this.comboBoxEmpleados_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(602, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(768, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(523, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Nombre:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(697, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Sueldo:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1366, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxEmpleados);
+            this.Controls.Add(this.buttonCalcular);
+            this.Controls.Add(this.dataGridViewResultado);
+            this.Controls.Add(this.buttonLeerDatos);
+            this.Controls.Add(this.dataGridViewEmpleados);
+            this.Controls.Add(this.dataGridViewAsistencia);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAsistencia)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmpleados)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResultado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewAsistencia;
+        private System.Windows.Forms.DataGridView dataGridViewEmpleados;
+        private System.Windows.Forms.Button buttonLeerDatos;
+        private System.Windows.Forms.DataGridView dataGridViewResultado;
+        private System.Windows.Forms.Button buttonCalcular;
+        private System.Windows.Forms.ComboBox comboBoxEmpleados;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabRepaso1
+{
+    public partial class Form1 : Form
+    {
+        //Cargar Empledos y Asistencia
+        List<Empleado> empleados = new List<Empleado>();
+        List<Asistencia> asistencias = new List<Asistencia>();
+        List<Resultado> resultados = new List<Resultado>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void CargarEmpleados()
+        {
+            //Leer el archivo y cargarlo a la lista
+            string fileName = "Empleados.txt";
+
+            //Abrimos el archivo, en este caso lo abrimos para lectura
+            FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+            //Un ciclo para leer el archivo hasta el final del archivo
+            while (reader.Peek() > -1)
+            {
+                //Leer los datos de un empleado
+                Empleado empleado = new Empleado();
+                empleado.NoEmpleado = Convert.ToInt16(reader.ReadLine());
+                empleado.Nombre = reader.ReadLine();
+                empleado.SueldoHora = Convert.ToDecimal(reader.ReadLine());
+
+                //Guardar el empleado a la lista de empleados
+                empleados.Add(empleado);
+            }
+            //Cerrar el archivo, esta linea es importante porque sino despues de correr varias veces el programa daría error de que el archivo quedó abierto muchas veces. Entonces es necesario cerrarlo despues de terminar de leerlo.
+            reader.Close();
+        }
+
+        public void MostrarEmpleados()
+        {
+            //Mostrar la lista de empleados en el DataGridView
+            dataGridViewEmpleados.DataSource = null;
+            dataGridViewEmpleados.DataSource = empleados;
+            dataGridViewEmpleados.Refresh();
+        }
+
+        public void CargarAsistencia()
+        {
+            //Leer el archivo y cargarlo a la lista
+            string fileName = "Asistencia.txt";
+
+            //Abrimos el archivo, en este caso lo abrimos para lectura
+            FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+            //Un ciclo para leer el archivo hasta el final del archivo
+            while (reader.Peek() > -1)
+            {
+                //Leer la asistencia de un empleado
+                Asistencia asistencia = new Asistencia();
+                asistencia.NoEmpleado = Convert.ToInt16(reader.ReadLine());
+                asistencia.HorasMes = Convert.ToInt16(reader.ReadLine());
+                asistencia.Mes = Convert.ToInt16(reader.ReadLine());
+
+                //Guardar la asistencia a la lista de asistencia
+                asistencias.Add(asistencia);
+            }
+            //Cerrar el archivo, esta linea es importante porque sino despues de correr varias veces el programa daría error de que el archivo quedó abierto muchas veces. Entonces es necesario cerrarlo despues de terminar de leerlo.
+            reader.Close();
+        }
+
+        public void MostrarAsistencia()
+        {
+            //Mostrar la lista de asistencia en el DataGridView
+            dataGridViewAsistencia.DataSource = null;
+            dataGridViewAsistencia.DataSource = asistencias;
+            dataGridViewAsistencia.Refresh();
+        }
+
+        private void buttonLeerDatos_Click(object sender, EventArgs e)
+        {
+            MostrarEmpleados();
+            MostrarAsistencia();
+        }
+
+        public void CargarResultados()
+        {
+            //Con este ciclo foreach se extraen los numeros de empleados para operar
+            foreach (Empleado empleado in empleados)
+            {
+                int noEmpleado = empleado.NoEmpleado;
+
+                foreach (Asistencia asistencia in asistencias)
+                {
+                    if (empleado.NoEmpleado == asistencia.NoEmpleado)
+                    {
+                        Resultado resultado = new Resultado();
+                        resultado.NombreEmpleado = empleado.Nombre;
+                        resultado.Mes = asistencia.Mes;
+                        resultado.SueldoMensual = empleado.SueldoHora * asistencia.HorasMes;
+
+                        resultados.Add(resultado);
+                    }
+                }
+            }
+        }
+
+        public void MostrarResultado()
+        {
+            //Mostrar la lista de resultado en el DataGridView
+            dataGridViewResultado.DataSource = null;
+            dataGridViewResultado.DataSource = resultados;
+            dataGridViewResultado.Refresh();
+        }
+
+        private void buttonCalcular_Click(object sender, EventArgs e)
+        {
+            CargarResultados();
+            MostrarResultado();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CargarEmpleados();
+            CargarAsistencia();
+            comboBoxEmpleados.DisplayMember = "Nombre";
+            comboBoxEmpleados.ValueMember = "noEmpleado";
+            comboBoxEmpleados.DataSource = empleados;
+
+        }
+
+        private void comboBoxEmpleados_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int noEmpleado = Convert.ToInt16(comboBoxEmpleados.SelectedValue);
+
+            //for(int i = 0; i < empleados.Count; i++)
+            //{
+            //    if(noEmpleado == empleados[i].NoEmpleado)
+            //    {
+            //        //Desplegar datos
+            //        label1.Text = empleados[i].Nombre;
+            //    }
+            //}
+
+            Empleado empleadoEncontrado = empleados.Find(c => c.NoEmpleado == noEmpleado);
+            Asistencia asistenciaEncontrada = asistencias.Find(c => c.NoEmpleado == noEmpleado);
+
+            decimal sueldo = empleadoEncontrado.SueldoHora * asistenciaEncontrada.HorasMes;
+
+            label1.Text = empleadoEncontrado.Nombre;
+            label2.Text = sueldo.ToString();
+        }
+    }
+}
